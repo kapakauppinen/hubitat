382597de9f25b9d052df83c6a4ddebbfc598e72c
M-Engelberg <43775033+M-Engelberg@users.noreply.github.com>
2022-10-21 10:35:01 +0200

    Update hubitatElectricityPrice.groovy

--- Drivers/ElectricityAPI/hubitatElectricityPrice.groovy
+++ Drivers/ElectricityAPI/hubitatElectricityPrice.groovy
@@ -1,34 +1,36 @@
 /*
- * Electricity price api
+ * Electricity price api 
 
  */
 import java.text.SimpleDateFormat
-
 
 metadata {
     definition(name: "Electricity price", namespace: "kapakauppinen", author: "Kari Kauppinen", importUrl: "") {
         capability "Sensor"
 		capability "Polling"
 		capability "Refresh"
-		attribute "PriceListToday", "HashMap"
+		attribute "PriceListToday", "HashMap"     
         attribute "CurrentPrice", "NUMBER"
-
-          command "clearStateVariables"
-
+	command "clearStateVariables" 
     }
 }
 
 preferences {
     section("Security") {
         input "securityToken", "text", title: "entsoe Token", required: true
+        // Area code can be found at 
+        // https://transparency.entsoe.eu/content/static_content/Static%20content/web%20api/Guide.html#_areas
+        // look for BZN + your zone 
+        // FI = 10YFI-1--------U
+        // SE3 = 10Y1001A1001A46L
+        input "areaCode", "text", title: "Area Code", required: true
         input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: true
-
     }
 }
 
 def clearStateVariables(){
     device.deleteCurrentState('PriceListToday')
-     state.clear()
+     state.clear()   
 }
 
 def logsOff() {
@@ -42,10 +44,8 @@ def updated() {
     if (logEnable) runIn(1800, logsOff)
 }
 
-
-
-def getParams()
-	{
+def getParams() 
+	{ 
         def date = new Date()
         def sdf = new SimpleDateFormat("yyyyMMdd")
 
@@ -55,10 +55,13 @@ def getParams()
 
         //documentation can be found
         // https://transparency.entsoe.eu/content/static_content/Static%20content/web%20api/Guide.html
+        // Area code is from: https://transparency.entsoe.eu/content/static_content/Static%20content/web%20api/Guide.html#_areas
+        // look for BZN|<your zone>
+        // Fi = 10YFI-1--------U
+        // SE3 = 10Y1001A1001A46L
 
-		"https://transparency.entsoe.eu/api?securityToken=${securityToken}&documentType=A44&in_Domain=10YFI-1--------U&out_Domain=10YFI-1--------U&periodStart=${start}0000&periodEnd=${end}2300"
+	    "https://transparency.entsoe.eu/api?securityToken=${securityToken}&documentType=A44&in_Domain=${areaCode}&out_Domain=${areaCode}&periodStart=${start}0000&periodEnd=${end}2300" 
 	}
-
 
 def poll () {
 
@@ -73,11 +76,10 @@ def poll () {
     def todayString =device.currentValue("PriceListToday").substring(1, device.currentValue("PriceListToday").length() - 1)
 
     //string to hasmap
-    for(String keyValue : todayString.split(",")) {
+    for(String keyValue : todayString.split(",")) {    
 
-        String[] pairs = keyValue.split("=", 2)
+        String[] pairs = keyValue.split("=", 2)    
         today.put(pairs[0].trim(), pairs.length == 1 ? "" : pairs[1].trim());
-
     }
 
     today = today.sort { it.key }
@@ -86,34 +88,32 @@ def poll () {
 
     if(x)
         sendEvent(name: "CurrentPrice", value: x.value)
-        if (logEnable)
+        if (logEnable) 
             log.debug  x.value
 }
 
-
 def refresh() {
-    if (logEnable)
-	 log.debug "Refresh"
+    if (logEnable) 
+	log.debug "Refresh"
 
 	def responseBody
 
 	try {
-
-		httpGet([uri: getParams(),contentType: "text/xml"]) { resp ->
+		
+		httpGet([uri: getParams(),contentType: "text/xml"]) { resp -> 
 			responseBody =  resp.getData() /*.TimeSeries[0].Period.Point[13].'price.amount'*/
-		}
+	    }
         //2022-10-13T22:00Z
         def pattern = "yyyy-MM-dd'T'HH:mm'Z'"
-        def outputTZ = TimeZone.getTimeZone('GMT+5')
+        def outputTZ = TimeZone.getTimeZone('GMT+3')
         def date =  Date.parse(pattern,responseBody.TimeSeries[0].Period.timeInterval.start.text().toString())
         def convertedDate = date.format("yyyyMMddHHmm", outputTZ)
         def timeserieDate
 
-
         //def hmap
         HashMap<String, String> today = new HashMap<String, String>()
 
-        Calendar calendar = new GregorianCalendar();
+        Calendar calendar = new GregorianCalendar();   
         def dateLabel
 
         //get the Timeseries from the Data
@@ -126,14 +126,14 @@ def refresh() {
 
                 //get the price (inc vat for each hour)
                 it.Period.Point.each {
-                    calendar.add(Calendar.HOUR, it.position.text().toString() as Integer);
+                    calendar.add(Calendar.HOUR, it.position.text().toString() as Integer); 
                     dateLabel = calendar.getTime().format("yyyyMMddHHmm", outputTZ)
                     calendar.setTime (timeserieDate)
-                    today.put(dateLabel, (Float.parseFloat(it.'price.amount'.text().toString())*0.124).round(2));
-
+                    // replace number in row below with your currancy factor
+                    today.put(dateLabel, (Float.parseFloat(it.'price.amount'.text().toString())*1.103).round(2));               
                 }
 
-                //log.debug
+                //log.debug     
 
             }
             catch (Exception ex) {
@@ -144,8 +144,8 @@ def refresh() {
 
 		sendEvent(name: "PriceListToday", value: today)
 
-
-	}
+		
+	} 
 	catch(Exception e) {
 		log.debug "error occured calling httpget ${e}"
 	}
